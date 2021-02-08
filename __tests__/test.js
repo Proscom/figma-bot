9e15c7b007e410b07538b8978854ff5ce9db8a87
Kirill Sviryukov <sviryukov.k@phystech.edu>
2021-02-08 16:38:53 +0300

    test: add __tests__/test.js with test server

---
+++ __tests__/test.js
@@ -0,0 +1,53 @@
+const { FigmaBotServer, saveURLToAirtable } = require('../lib');
+
+const server = new FigmaBotServer({
+  postRequestHandlerScripts: {
+    createFile: (req, res, result) =>
+      saveURLToAirtable(req, res, {
+        baseURL: 'https://www.figma.com/file',
+        itemId: result
+      })
+  },
+  responseWaitForFileCreated: false,
+  responseWaitForProjectCreated: false
+});
+(async () => {
+  await server.start();
+  server.app.post(
+    '/createProjectAndFile',
+    server.authMiddleware,
+    async (req, res) => {
+      const projectName = req.query.projectName;
+      const fileName = req.query.fileName;
+      const teamId = req.query.teamId;
+      if (!projectName || typeof projectName !== 'string') {
+        res
+          .status(422)
+          .send('Query parameter "projectName" must be a non-empty string.');
+        return;
+      }
+      if (!fileName || typeof fileName !== 'string') {
+        res
+          .status(422)
+          .send('Query parameter "fileName" must be a non-empty string.');
+        return;
+      }
+      if (!teamId || typeof teamId !== 'string') {
+        res
+          .status(422)
+          .send('Query parameter "teamId" must be a non-empty string.');
+        return;
+      }
+      try {
+        const projectId = await server.bot.createProject(projectName, teamId);
+        const fileId = await server.bot.createFile(fileName, projectId);
+        await saveURLToAirtable(req, res, {
+          baseURL: 'https://www.figma.com/file',
+          itemId: fileId
+        });
+      } catch (e) {
+        req.log.error(e.message);
+      }
+    }
+  );
+})();
