dd8664e8f37e1e5408594289e986698cad551a80
Kirill Sviryukov <sviryukov.k@phystech.edu>
2021-02-11 14:46:24 +0300

    refactor: fix error messages

--- src/utils.ts
+++ src/utils.ts
@@ -100,21 +100,21 @@ export const goToTeamPage = async (page: Page, teamId: string) => {
   try {
     await goTo(page, `https://www.figma.com/files/team/${teamId}`);
   } catch {
-    throw new Error(`Team with id ${teamId} page loading failed.`);
+    throw new Error(`Team with id "${teamId}" page loading failed.`);
   }
 };
 export const goToProjectPage = async (page: Page, projectId: string) => {
   try {
     await goTo(page, `https://www.figma.com/files/project/${projectId}`);
   } catch {
-    throw new Error(`Project with id ${projectId} page loading failed.`);
+    throw new Error(`Project with id "${projectId}" page loading failed.`);
   }
 };
 export const goToFilePage = async (page: Page, fileId: string) => {
   try {
     await goTo(page, `https://www.figma.com/file/${fileId}`);
   } catch {
-    throw new Error(`File with id ${fileId} page loading failed.`);
+    throw new Error(`File with id "${fileId}" page loading failed.`);
   }
 };
 
