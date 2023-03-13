8b7bc0e30a536c964991b18ac9c99cd6073a57ba
mirakl-026 <evgeniy.pgm@inbox.ru>
2023-03-13 11:46:58 +0300

    [refactor] move cookieProvider, errors and util functions to /common

---
+++ src/common/functions.ts
@@ -0,0 +1,5 @@
+export const random = (min: number, max: number) =>
+  min + Math.round(Math.random() * (max - min));
+
+export const wait = (timeout: number) =>
+  new Promise<void>((resolve) => setTimeout(resolve, timeout));
