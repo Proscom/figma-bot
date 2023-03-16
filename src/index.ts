8016fbfa230b3aae942922ee5c1dab8536fe50de
Mirakl-026 <e.bushin@proscom.ru>
2023-03-16 17:00:28 +0300

    [fix] fix export with FigmaBot filename

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export * from './common/errors';
 export * from './common/functions';
 export * from './figmaBot.common.actions';
 export * from './figmaBot.interfaces';
-export * from './figmaBot';
+export * from './FigmaBot';
